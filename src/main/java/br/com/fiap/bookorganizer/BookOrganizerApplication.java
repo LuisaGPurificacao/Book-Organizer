39391375c51aed4cbf5967615bc2b68f8dae729a
kellerlivia <rm94275@fiap.com.br>
2023-03-06 22:52:56 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/fiap/bookorganizer/BookOrganizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/bookorganizer/BookOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
