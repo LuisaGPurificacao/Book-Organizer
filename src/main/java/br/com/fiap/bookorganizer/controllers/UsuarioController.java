72ff527b32725f7b22e664db48b40927ab09186d
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-03-10 20:29:50 -0300

    Atualização UsuarioController

--- src/main/java/br/com/fiap/bookorganizer/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/bookorganizer/controllers/UsuarioController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.bookorganizer.models.Autor;
+import br.com.fiap.bookorganizer.models.Categoria;
 import br.com.fiap.bookorganizer.models.Livro;
 import br.com.fiap.bookorganizer.models.Usuario;
 
@@ -15,10 +17,12 @@ public class UsuarioController {
     
     @GetMapping("book-organizer/usuarios")
     public Usuario show(){
+        Autor autor = new Autor(1, "João Carlos");
+        Categoria categoria = new Categoria(1, "Tecnologia");
         byte[] fotoPerfil = HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d");
         List<Livro> livros = new ArrayList<>();
-        livros.add(new Livro(1, "Java", 200, 5, 100));
-        livros.add(new Livro(2, "Spring", 250, 5, 25));
+        livros.add(new Livro(1, "Java", 200, 5, 100, categoria, autor));
+        livros.add(new Livro(2, "Spring", 250, 5, 25, categoria, autor));
         return new Usuario(1, "Lívia Keller", "kellerlivia@gmail.com", "carvalho123", fotoPerfil, 5, 20, livros);
     }
 
