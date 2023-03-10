3bc4858952512f71194d96b0dae2d3815f8d39d1
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-03-10 20:29:27 -0300

    Adicionando relacionamentos com categoria e autor

--- src/main/java/br/com/fiap/bookorganizer/controllers/LivroController.java
+++ src/main/java/br/com/fiap/bookorganizer/controllers/LivroController.java
@@ -3,6 +3,8 @@ package br.com.fiap.bookorganizer.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.bookorganizer.models.Autor;
+import br.com.fiap.bookorganizer.models.Categoria;
 import br.com.fiap.bookorganizer.models.Livro;
 
 @RestController
@@ -10,6 +12,6 @@ public class LivroController {
     
     @GetMapping("book-organizer/livros")
     public Livro show(){
-        return new Livro(1, "Clean Code", 500, 5, 100);
+        return new Livro(1, "Clean Code", 500, 5, 100, new Categoria(2, "Tecnologia"), new Autor(1, "João Carlos"));
     }
 }
