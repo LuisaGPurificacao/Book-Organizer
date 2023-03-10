30d82be04d2e7dfc11e3a64b28d68d54d15a7631
kellerlivia <rm94275@fiap.com.br>
2023-03-10 19:55:30 -0300

    categoria model e controller

---
+++ src/main/java/br/com/fiap/bookorganizer/models/Categoria.java
@@ -0,0 +1,30 @@
+package br.com.fiap.bookorganizer.models;
+
+public class Categoria {
+
+    private long id;
+    private String nome;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria [id=" + id + ", nome=" + nome + "]";
+    }
+}
