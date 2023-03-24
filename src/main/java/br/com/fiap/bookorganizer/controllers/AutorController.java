0547f4bfac0b81b9a9ba32649c82839af6d1587a
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-03-24 18:44:06 -0300

    AutorController com integração no banco

--- src/main/java/br/com/fiap/bookorganizer/controllers/AutorController.java
+++ src/main/java/br/com/fiap/bookorganizer/controllers/AutorController.java
@@ -1,10 +1,11 @@
 package br.com.fiap.bookorganizer.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,31 +17,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.bookorganizer.models.Autor;
+import br.com.fiap.bookorganizer.repositories.AutorRepository;
 
 @RestController
 public class AutorController {
 
     Logger log = LoggerFactory.getLogger(LivroController.class);
 
-    List<Autor> autores = new ArrayList<>();
+    @Autowired
+    AutorRepository repository;
 
     @GetMapping("book-organizer/autores")
     public List<Autor> index(){
-        return autores;
+        return repository.findAll();
     }
     
     @PostMapping("book-organizer/autores")
     public ResponseEntity<Autor> create(@RequestBody Autor autor){
         log.info("cadastrando autor: " + autor);
-        autor.setId(autores.size() + 1);
-        autores.add(autor);
+        repository.save(autor);
         return ResponseEntity.status(HttpStatus.CREATED).body(autor);
     }
 
     @GetMapping("book-organizer/autores/{id}")
     public ResponseEntity<Autor> show(@PathVariable Integer id){
         log.info("buscando autor com id: " + id);
-        var optionalAutor = autores.stream().filter(d -> d.getId().equals(id)).findFirst();
+        Optional<Autor> optionalAutor = repository.findById(id);
 
         if (optionalAutor.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -51,12 +53,11 @@ public class AutorController {
     @DeleteMapping("book-organizer/autores/{id}")
     public ResponseEntity<Autor> delete(@PathVariable Integer id){
         log.info("apagando autor com id: " + id);
-        var optionalAutor = autores.stream().filter(d -> d.getId().equals(id)).findFirst();
 
-        if (optionalAutor.isEmpty())
+        if (!repository.existsById(id))
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
-            autores.remove(optionalAutor.get());
+        repository.deleteById(id);
         
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
@@ -64,14 +65,13 @@ public class AutorController {
     @PutMapping("book-organizer/autores/{id}")
     public ResponseEntity<Autor> update(@PathVariable Integer id, @RequestBody Autor autor){
         log.info("atualizando autor com id: " + id);
-        var optionalAutor = autores.stream().filter(d -> d.getId().equals(id)).findFirst();
+        var optionalAutor = repository.findById(id);
 
         if (optionalAutor.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
-        autores.remove(optionalAutor.get());
         autor.setId(id);
-        autores.add(autor);
+        repository.save(autor);
         
         return ResponseEntity.status(HttpStatus.OK).body(autor);
     }
--- src/main/java/br/com/fiap/bookorganizer/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/bookorganizer/controllers/UsuarioController.java
@@ -1,6 +1,5 @@
 package br.com.fiap.bookorganizer.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
