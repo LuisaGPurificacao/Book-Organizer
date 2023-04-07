55b30ed46d58b3043d97ce2a9f3f4f1335d7ecf9
kellerlivia <rm94275@fiap.com.br>
2023-04-07 10:45:04 -0300

    lombok and relation livro-categoria

--- src/main/java/br/com/fiap/bookorganizer/models/Livro.java
+++ src/main/java/br/com/fiap/bookorganizer/models/Livro.java
@@ -4,11 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Livro {
     
@@ -32,79 +37,11 @@ public class Livro {
     @NotNull (message = "A página atual é obrigatória")
     @Positive(message = "A página atual precisa ser um número positivo")
     private int paginaAtual;
-    //private Categoria categoria;
-    //private Autor autor;
 
-    protected Livro() {}
+    @ManyToOne
+    private Categoria categoria;
+    
+    @ManyToOne
+    private Autor autor;
 
-    public Livro(Long id, String titulo, int quantidadePaginas, int avaliacao, int paginaAtual, Categoria categoria, Autor autor) {
-        this.id = id;
-        this.titulo = titulo;
-        this.quantidadePaginas = quantidadePaginas;
-        this.avaliacao = avaliacao;
-        this.paginaAtual = paginaAtual;
-        //this.categoria = categoria;
-        //this.autor = autor;
-        setStatus(quantidadePaginas, paginaAtual);
-    }
-
-    @Override
-    public String toString() {
-        return "Livro [id=" + id + ", titulo=" + titulo + ", quantidadePaginas=" + quantidadePaginas + ", avaliacao="
-                + avaliacao + ", status=" + status + ", paginaAtual=" + paginaAtual + "]";
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public int getQuantidadePaginas() {
-        return quantidadePaginas;
-    }
-    public void setQuantidadePaginas(int quantidadePaginas) {
-        this.quantidadePaginas = quantidadePaginas;
-    }
-    public int getAvaliacao() {
-        return avaliacao;
-    }
-    public void setAvaliacao(int avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-    public double getStatus() {
-        return status;
-    }
-    public void setStatus(int quantidadePaginas, int paginaAtual) {
-        this.status = ((double)paginaAtual *100) / (double)quantidadePaginas;
-    }
-    public int getPaginaAtual() {
-        return paginaAtual;
-    }
-    public void setPaginaAtual(int paginaAtual) {
-        this.paginaAtual = paginaAtual;
-    }
-/* 
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    public Autor getAutor() {
-        return autor;
-    }
-
-    public void setAutor(Autor autor) {
-        this.autor = autor;
-    }
-*/    
 }
